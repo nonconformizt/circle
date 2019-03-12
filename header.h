49dd2dacb003fc42ff1085797d309fef18e6bfac
Nonconformizt <bogdankorzh254@mail.ru>
2019-03-12 21:28:46 +0300

    FPS control provided

--- header.h
+++ header.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <time.h>
+#include <chrono>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
@@ -11,6 +12,9 @@ using namespace std;
 #define WINDOW_WIDTH (960)
 #define WINDOW_HEIGHT (720)
 
+#define START_X (WINDOW_WIDTH / 2)
+#define START_Y (WINDOW_HEIGHT / 2)
+#define RADIUS 40
 #define SPEED 10
 
 void SDL_DrawCircle(SDL_Renderer *Renderer, int32_t _x, int32_t _y, int32_t radius);
--- main.cpp
+++ main.cpp
@@ -2,15 +2,44 @@
 
 
 int main(int argc, char ** argv) {
-
     SDL_Window * win;
     SDL_Renderer * rend;
 
     SDL_Start( &win, &rend );
 
-    SDL_RenderClear( rend );
-    SDL_SetRenderDrawColor( rend, 255, 255, 255, 0 );
-    SDL_DrawCircle( rend, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 30 );
-    SDL_RenderPresent( rend );
-    SDL_Delay( 5000 );
+    SDL_Event e;
+    bool close = false,
+         pause = false;
+    double x = START_X,
+           y = START_Y,
+           x_vel = 0,
+           y_vel = 0;
+
+    while (!close) {
+        auto begin = chrono::high_resolution_clock::now();
+        while (SDL_PollEvent(&e))
+            if ( e.type == SDL_QUIT || e.key.keysym.sym == SDLK_ESCAPE )
+                close = true;
+            else if ( e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_p )
+                pause = true;
+
+
+        SDL_SetRenderDrawColor( rend, 0, 0, 0, 0 );
+        SDL_RenderClear( rend );
+
+        x += x_vel;
+        y += y_vel;
+
+        SDL_SetRenderDrawColor( rend, 255, 255, 255, 0 );
+        SDL_DrawCircle( rend, int(x), int(y), RADIUS );
+        SDL_RenderPresent( rend );
+
+        
+
+        auto end = chrono::high_resolution_clock::now();
+        auto dur = end - begin;
+        auto dur_ms = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+        SDL_Delay( 16 - Uint32(dur_ms) );
+    }
+
 }
